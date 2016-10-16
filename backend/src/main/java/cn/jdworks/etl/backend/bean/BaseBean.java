ba05be18a68d7e175c54fcde57fd95c182fd5db5
Lixin <lixin@ngblab.org>
2016-10-17 07:53:42 +0800

    add shiro

--- backend/src/main/java/cn/jdworks/etl/backend/bean/BaseBean.java
+++ backend/src/main/java/cn/jdworks/etl/backend/bean/BaseBean.java
@@ -1,34 +1,51 @@
 package cn.jdworks.etl.backend.bean;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.EL;
+import org.nutz.dao.entity.annotation.Prev;
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
+import org.nutz.lang.random.R;
 
-public abstract class BaseBean {
+public abstract class BaseBean  implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Prev(els=@EL("$me.now()"))
 	@Column("ct")
-    protected Date createTime;
-    @Column("ut")
-    protected Date updateTime;
+	protected Date createTime;
+	@Prev(els=@EL("$me.now()"))
+	@Column("ut")
+	protected Date updateTime;
+	
+	public String toString() {
+		return String.format("/*%s*/%s", super.toString(), Json.toJson(this, JsonFormat.compact()));
+	}
 
-    public String toString() {
-        return String.format("/*%s*/%s", super.toString(), Json.toJson(this, JsonFormat.compact()));
-    }
+	public Date getCreateTime() {
+		return createTime;
+	}
 
-    public Date getCreateTime() {
-        return createTime;
-    }
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
 
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
+	public Date getUpdateTime() {
+		return updateTime;
+	}
 
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+	
+	public Date now() {
+		return new Date();
+	}
+	
+	public String uuid() {
+		return R.UU32().toLowerCase();
+	}
 }
